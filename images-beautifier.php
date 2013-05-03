9ffdfaadfd42f39bf3011e7492e30e37c86b847a
satrya <satrya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-03 07:05:41 +0000

    Release
    git-svn-id: https://plugins.svn.wordpress.org/images-beautifier/trunk@707293 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ images-beautifier.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ * Plugin Name: Images Beautifier
+ * Plugin URI: https://github.com/satrya/images-beautifier
+ * Description: The easiest way to beautify your images with simple shortcode.
+ * Version: 1.0
+ * Author: Satrya
+ * Author URI: http://satrya.me
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the License, 
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write 
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @author    Satrya
+ * @copyright Copyright (c) 2013, Satrya
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Images_Beautifier {
+
+	/**
+	 * PHP5 constructor method.
+	 *
+	 * @since 1.0
+	 */
+	public function __construct() {
+
+		add_action( 'plugins_loaded', array( &$this, 'constants' ), 1 );
+
+		add_action( 'init', array( &$this, 'init' ) );
+
+	}
+
+	/**
+	 * Defines constants used by the plugin.
+	 *
+	 * @since 1.0
+	 */
+	public function constants() {
+
+		define( 'IB_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+
+	}
+
+	/**
+	 * Sets up actions/filters.
+	 *
+	 * @since 1.0
+	 */
+	public function init() {
+
+		/* Enqueue stylesheets on 'wp_enqueue_scripts'. */
+		add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_styles' ) );
+
+		/* Register shortcodes. */
+		add_shortcode( 'image', array( &$this, 'setup_shortcode' ) );
+
+		/* Make text widgets shortcode aware. */
+		add_filter( 'widget_text', 'do_shortcode' );
+
+	}
+
+	/**
+	 * Enqueue stylesheet.
+	 *
+	 * @since 1.0
+	 */
+	public function enqueue_styles() {
+		wp_enqueue_style( 'images-beautifier-style', IB_URI . 'images.css', false, '1.0' );
+	}
+
+	/**
+	 * Setup the image shortcode.
+	 *
+	 * @since 1.0
+	 */
+	public function setup_shortcode( $atts, $content = "" ) {
+		extract( shortcode_atts( array(
+			'type'   => 'normal'
+	    ), $atts) );
+	
+		return '<span class="img-beautifier img-' . sanitize_html_class( $type ) . '">' . $content . '</span>';
+	}
+
+}
+
+new Images_Beautifier();
+?>
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Images Beautifier ===
+Contributors: satrya
+Donate link: http://satrya.me/donate/
+Tags: shortcode, image, images, posts, page, widget
+Requires at least: 3.4
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The easiest way to beautify your images with simple shortcode.
+
+== Description ==
+
+The easiest way to beautify your images with simple shortcode. Any image placed inside the shortcode will be wrapped with custom style provided by the plugin.
+
+= Usage =
+`[image type=""] ... [/image]`
+
+`
+Optional arguments:
+* type: rounded, beauty, polaroid, circle
+`
+
+= Support =
+* [Support forum](http://wordpress.org/support/plugin/images-beautifier).
+* [Github repo](https://github.com/satrya/images-beautifier).
+
+= Project Info =
+* Developed by [Satrya](http://satrya.me/).
+
+= Credit =
+* [Twitter bootstrap images](http://twitter.github.io/bootstrap/base-css.html#images) style.
+
+== Installation ==
+
+1. Upload the 'images-beautifier' folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Display the shortcode with `[image] ... [/image]`
+
+== Screenshots ==
+1. Preview
+
+== Changelog ==
+
+= 1.0 - 5/01/2013 =
+* Initial release
